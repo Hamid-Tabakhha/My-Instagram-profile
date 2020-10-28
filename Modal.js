02255d83e6a732a234cba5df9df4a3ec44feee72
connor77 <hamiid1377@gmail.com>
2020-10-28 17:45:58 +0330

    change sources to local

--- Modal.js
+++ Modal.js
@@ -1,10 +1,11 @@
-
 var modal = document.getElementById("myModal");
 var img = document.getElementById("myImg");
 var modalImg = document.getElementById("img01");
 var span = document.getElementsByClassName("close")[0];
 
 $(document).ready(function () {
+  $(".img-fluid").on
+
   span.onclick = function () {
     modal.style.display = "none";
   };
